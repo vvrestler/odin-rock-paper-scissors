1fe79f6e4b2af86d248a2f2ddcafa5969e973678
gary <110794817+vvrestler@users.noreply.github.com>
2022-11-29 19:57:54 -0500

    Hide buttons after game ends

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => playRound("ROCK"));
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => playRound("PAPER"));
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => playRound("SCISSORS"));
+
+const results = document.querySelector('#results');
+const score = document.querySelector('#score');
+
 function getComputerChoice() {
     const options = ["ROCK", "PAPER", "SCISSORS"];
     const choice = Math.floor(Math.random() * options.length);
@@ -16,13 +28,13 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
                 break;
             case ("PAPER"):
                 results.textContent = "You won this round!";
-                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 playerScore++;
+                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 break;
             case ("SCISSORS"):
                 results.textContent = "You lost this round.";
-                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 computerScore++;
+                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 break;
             default:
                 alert("Something has gone wrong.");
@@ -32,8 +44,8 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
         switch (playerSelection) {
             case ("ROCK"):
                 results.textContent = "You lost this round.";
-                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 computerScore++;
+                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 break;
             case ("PAPER"):
                 results.textContent = "Tie! Play again.";
@@ -41,8 +53,8 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
                 break;
             case ("SCISSORS"):
                 results.textContent = "You won this round!";
-                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 playerScore++;
+                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 break;
             default:
                 alert("Something has gone wrong.");
@@ -52,31 +64,36 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
         switch (playerSelection) {
             case ("ROCK"):
                 results.textContent = "You won this round!";
-                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 playerScore++;
+                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 break;
             case ("PAPER"):
                 results.textContent = "You lost this round.";
-                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 computerScore++;
+                score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
                 break;
             case ("SCISSORS"):
                 results.textContent = "Tie! Play again.";
                 score.textContent = `Your score is ${playerScore} and the computer's score is ${computerScore}.`
+                break;
             default:
                 alert("Something has gone wrong.");
             }
-    }     
+    }
+    
+    if(playerScore == 5 && !(computerScore >= 5)) {
+        const win = document.createElement('p');
+        win.textContent = 'You won the game! Congratulations!';
+        score.appendChild(win);
+            rock.style.display = 'none';
+            paper.style.display = 'none';
+            scissors.style.display = 'none';
+    } else if(computerScore == 5 && !(playerScore >= 5)) {
+        const lose = document.createElement('p');
+        lose.textContent = 'You lost the game. Better luck next time!';
+        score.appendChild(lose);
+            rock.style.display = 'none';
+            paper.style.display = 'none';
+            scissors.style.display = 'none';
+    }
 }
-
-const rock = document.querySelector('#rock');
-rock.addEventListener('click', () => playRound("ROCK"));
-
-const paper = document.querySelector('#paper');
-paper.addEventListener('click', () => playRound("PAPER"));
-
-const scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', () => playRound("SCISSORS"));
-
-const results = document.querySelector('#results');
-const score = document.querySelector('#score');
